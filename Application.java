a0c4fde4b5b02d1c0f964745c17361385f4f54ff
IgnitionOrk <c3179234@uon.edu.au>
2018-05-07 23:11:46 +1000

    Cleaning code

--- Application.java
+++ Application.java
@@ -14,6 +14,7 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 
@@ -22,133 +23,103 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			
-			
-			/**
-			 * To do list:
-			 * encryption: input file
-			 * encryption: output file:
-			 * 
-			 * 
-			 * decryption: input file: Contains the encrypted text, and key. 
-			 * decryption: output file: Contains ciphertext, Key, and the decrypted plaintext.
-			 * 
-			 * 
-			 * */
-
-			System.out.println("Loading...");
-			System.out.println("Reading plaintext and key from file "+args[0]);
-			
+		try {	
 			Scanner scanner = new Scanner(new File(args[0]));
-			String plaintext = scanner.nextLine();
+			String text = scanner.nextLine();
 			String key = scanner.nextLine();
-
-			String p[] = new String[64];
-			String k[] = new String[64];
-			String bit="0";
-
-			for(int i=0; i<64; i++)
-			{
-				p[i]=plaintext;
-				k[i]=key;
-
-				if(p[i].charAt(i)=='0')
-				{
-					bit="1";
-				}else{
-					bit="0";
-				}
-				p[i]=p[i].substring(0,i)+bit+p[i].substring(i+1);
-
-				if(i<56)
-					{
-					if(k[i].charAt(i)=='0')
-					{
-						bit="1";
-					}else{
-						bit="0";
-					}
-					k[i]=k[i].substring(0,i)+bit+k[i].substring(i+1);
-				}
+			if(args[0].toLowerCase().contains("decryption")){
+				decryptFile(text, key);
+			}
+			else{
+				encryptFile(text, key);
 			}
 
-			System.out.println("Starting encryption...");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * @param scanner 
+	 * 
+	 */
+	private static void encryptFile(String plaintext, String key){
+		String p[] = bitDifferenceArray(plaintext); // An array of plaintexts differing by 1-bit.
+		String k[] = bitDifferenceArray(key);	// An array of keys differing by 1-bit.
+		/**
+		P and Pi under K, DES0/1/2/3
+		*/
+		DES des0Normal=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, key);
+		DES des1Normal=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, key);
+		DES des2Normal=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, key);
+		DES des3Normal=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, key);
 
-			/**
-			P and Pi under K, DES0/1/2/3
-			*/
-			DES des0Normal=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, key);
-			DES des1Normal=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, key);
-			DES des2Normal=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, key);
-			DES des3Normal=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, key);
+		des0Normal.encrypt();
+		des1Normal.encrypt();
+		des2Normal.encrypt();
+		des3Normal.encrypt();
 
-			des0Normal.encrypt();
-			des1Normal.encrypt();
-			des2Normal.encrypt();
-			des3Normal.encrypt();
+		DES des0[] = new DES[64];
+		DES des1[] = new DES[64];
+		DES des2[] = new DES[64];
+		DES des3[] = new DES[64];
 
-			DES des0[] = new DES[64];
-			DES des1[] = new DES[64];
-			DES des2[] = new DES[64];
-			DES des3[] = new DES[64];
+		for(int i=0; i<64; i++)
+		{
+			des0[i]=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), p[i], key); //encrypt P and Pi{1-63} under K
+			des0[i].encrypt();
+			des1[i]=new DES("DES1", new Round(new int[]{0, 1, 2}), p[i], key); //encrypt P and Pi{1-63} under K
+			des1[i].encrypt();
+			des2[i]=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), p[i], key); //encrypt P and Pi{1-63} under K
+			des2[i].encrypt();
+			des3[i]=new DES("DES3", new Round(new int[]{0, 1, 4}), p[i], key); //encrypt P and Pi{1-63} under K
+			des3[i].encrypt();
+		}
 
-			for(int i=0; i<64; i++)
-			{
-				des0[i]=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), p[i], key); //encrypt P and Pi{1-63} under K
-				des0[i].encrypt();
-				des1[i]=new DES("DES1", new Round(new int[]{0, 1, 2}), p[i], key); //encrypt P and Pi{1-63} under K
-				des1[i].encrypt();
-				des2[i]=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), p[i], key); //encrypt P and Pi{1-63} under K
-				des2[i].encrypt();
-				des3[i]=new DES("DES3", new Round(new int[]{0, 1, 4}), p[i], key); //encrypt P and Pi{1-63} under K
-				des3[i].encrypt();
-			}
+		/**
+		P under K and Ki, DES0/1/2/3
+		*/
 
-			/**
-			P under K and Ki, DES0/1/2/3
-			*/
+		DES desK0Normal=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, key);
+		DES desK1Normal=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, key);
+		DES desK2Normal=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, key);
+		DES desK3Normal=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, key);
 
-			DES desK0Normal=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, key);
-			DES desK1Normal=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, key);
-			DES desK2Normal=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, key);
-			DES desK3Normal=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, key);
+		desK0Normal.encrypt();
+		desK1Normal.encrypt();
+		desK2Normal.encrypt();
+		desK3Normal.encrypt();
 
-			desK0Normal.encrypt();
-			desK1Normal.encrypt();
-			desK2Normal.encrypt();
-			desK3Normal.encrypt();
+		DES desK0[] = new DES[64];
+		DES desK1[] = new DES[64];
+		DES desK2[] = new DES[64];
+		DES desK3[] = new DES[64];
+		for(int i=0; i<56; i++)
+		{
+			desK0[i]=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
+			desK0[i].encrypt();
+			desK1[i]=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
+			desK1[i].encrypt();
+			desK2[i]=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
+			desK2[i].encrypt();
+			desK3[i]=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
+			desK3[i].encrypt();
+		}
 
-			DES desK0[] = new DES[64];
-			DES desK1[] = new DES[64];
-			DES desK2[] = new DES[64];
-			DES desK3[] = new DES[64];
-			for(int i=0; i<56; i++)
-			{
-				desK0[i]=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
-				desK0[i].encrypt();
-				desK1[i]=new DES("DES1", new Round(new int[]{0, 1, 2}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
-				desK1[i].encrypt();
-				desK2[i]=new DES("DES2", new Round(new int[]{0, 1, 4, 3}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
-				desK2[i].encrypt();
-				desK3[i]=new DES("DES3", new Round(new int[]{0, 1, 4}), plaintext, k[i]); //encrypt P and Pi{1-63} under K
-				desK3[i].encrypt();
-			}
+		/**
+		finished initialising all the DES objects
+		*/
 
-			/**
-			finished initialising all the DES objects
-			*/
-
-			System.out.println("Done!\n");
-
-			System.out.println("ENCRYPTION");
-			System.out.println("Plaintext P:\t" + plaintext);
-			System.out.println("Key K:\t\t" + key);
-			System.out.println("Ciphertext C:\t"+ des0Normal.getCiphertext());
+		try{
+		    PrintWriter eWriter = new PrintWriter("ENCRYPTIONOUTPUT.txt", "UTF-8");
+		    
+		    eWriter.write("ENCRYPTION\n");
+		    eWriter.write("Plaintext P:" + plaintext+"\n");
+		    eWriter.write("Key K:" + key+"\t\n");
+		    eWriter.write("Ciphertext C:"+ des0Normal.getCiphertext()+"\n");
 
 
-			System.out.println("\nAvalanche effect: P and Pi under K\nRound:\tDES0\tDES1\tDES2\tDES3");
-			System.out.println("0\t1\t1\t1\t1");
+		    eWriter.write("\nAvalanche:\nP and Pi under K\nRound:\tDES0\tDES1\tDES2\tDES3\n");
+		    eWriter.write("0\t\t1\t\t1\t\t1\t\t1\n");
 			for(int i=0; i<16; i++)
 			{
 				double avalanche0=1;
@@ -167,11 +138,11 @@ public class Application {
 				int a2=(int)Math.round(avalanche2/64.0); //take the average
 				int a3=(int)Math.round(avalanche3/64.0); //take the average
 
-				System.out.println((i+1)+"\t"+a0+"\t"+a1+"\t"+a2+"\t"+a3);
+				eWriter.write((i+1)+"\t\t"+a0+"\t\t"+a1+"\t\t"+a2+"\t\t"+a3+"\n");
 			}
 
-			System.out.println("\nAvalanche effect: P under K and Ki\nRound:\tDES0\tDES1\tDES2\tDES3");
-			System.out.println("0\t1\t1\t1\t1");
+			eWriter.write("\nP under K and Ki\nRound:\tDES0\tDES1\tDES2\tDES3\n");
+			eWriter.write("0\t\t1\t\t1\t\t1\t\t1\n");
 			for(int i=0; i<16; i++)
 			{
 				double avalanche0=1;
@@ -190,28 +161,65 @@ public class Application {
 				int a2=(int)Math.round(avalanche2/56.0); //take the average
 				int a3=(int)Math.round(avalanche3/56.0); //take the average
 
-				System.out.println((i+1)+"\t"+a0+"\t"+a1+"\t"+a2+"\t"+a3);
+				eWriter.write((i+1)+"\t\t"+a0+"\t\t"+a1+"\t\t"+a2+"\t\t"+a3+"\n");
 			}
+			eWriter.close();
 
-
+			
+		} catch (Exception e) {}	
+	}
+	/**
+	 * Calculates the avalanche effect for each of the, 
+	 * @return
+	 */
+	private String avalanche(){
+		return "";
+	}
+	
+	/**
+	 * @param text
+	 * @return
+	 */
+	private static String[] bitDifferenceArray(String text){
+		String[] bitDifference = new String[64];
+		for(int i=0; i<text.length(); i++){
+			bitDifference[i] = text;
+			if(bitDifference[i].charAt(i)=='0'){
+				bitDifference[i]=bitDifference[i].substring(0,i)+"1"+bitDifference[i].substring(i+1);
+			}
+			else{
+				bitDifference[i]=bitDifference[i].substring(0,i)+"0"+bitDifference[i].substring(i+1);
+			}
+		}
+		return bitDifference;
+	}
+	
+	/**
+	 * @param scanner 
+	 * 
+	 */
+	private static void decryptFile(String ciphertext, String key){
+		try{
+		    PrintWriter dWriter = new PrintWriter("DECRYPTIONOUTPUT.txt", "UTF-8");
 			//DECRYPTION, read first line as ciphertext, second line as key
-			DES des0Decrypt=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), "", key, plaintext); //version, round, plaintext, key, ciphertext
-			des0Decrypt.decrypt();
-
-			System.out.println("\n\nDECRYPTION");
-			System.out.println("Ciphertext C:\t" + des0Decrypt.getCiphertext());
-			System.out.println("Key K:\t\t" + key);
-			System.out.println("Plaintext P:\t"+ des0Decrypt.getPlaintext());
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			DES des0Decrypt=new DES("DES0", new Round(new int[]{0, 1, 2, 3}), "", key, ciphertext); //version, round, plaintext, key, ciphertext
+			des0Decrypt.decrypt();		
+			dWriter.write("DECRYPTION\n");
+			dWriter.write("Ciphertext C:" + des0Decrypt.getCiphertext()+"\n");
+			dWriter.write("Key K:" + key+"\n");
+			dWriter.write("Plaintext P:"+ des0Decrypt.getPlaintext()+"\n");
+			dWriter.close();
+		}
+		catch(Exception ex){
+			
 		}
 	}
 	
-	private static void fileWriter(){
-		
-	}
-
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	private static int checkDifferences(String a, String b)
 	{
 		int differenceCount=0;
