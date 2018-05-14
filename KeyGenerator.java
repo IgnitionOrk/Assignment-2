c357a29f3e88a1acdb494575c9a46b9a4699505b
IgnitionOrk <c3179234@uon.edu.au>
2018-05-14 23:01:26 +1000

    Just little additions

--- KeyGenerator.java
+++ KeyGenerator.java
@@ -12,11 +12,11 @@ public class KeyGenerator {
 	private final int[] PC1 = new int[]{57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
 	private final int[] PC2 = new int[]{14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
 	private final int PADKEYLEGNTH = 64;
-	private final char BIT = '1';
+	private final String BIT = "1";
+	private final String COMPLEMENT = "0";
 	/**
 	 * @param key 
 	 * @param b 
-	 * 
 	 */
 	public KeyGenerator(boolean decrypting, String key){
 		this.iCount = 0;
@@ -41,7 +41,7 @@ public class KeyGenerator {
 		}
 		iCount = 0;
 		key = padded(key);
-		key = Transposition.permutation(key, PC1);
+		key = Transposition.permute(key, PC1);
 		subkeys(key);
 	}
 	/**
@@ -55,7 +55,7 @@ public class KeyGenerator {
 		for(int lowerBound = 0, upperBound = eLength; lowerBound < key.length() / eLength; lowerBound++, upperBound += eLength){
 			sevenBits = key.substring(lowerBound * eLength, upperBound);
 			// initial seven bits, but with an additional bit, return from the xor function. 
-			padded += sevenBits+xor(sevenBits);
+			padded += sevenBits+eighthBit(sevenBits);
 		}
 		return padded;
 	}
@@ -63,24 +63,13 @@ public class KeyGenerator {
 	 * @param sevenBits
 	 * @return
 	 */
-	private String xor(String sevenBits){
+	private String eighthBit(String sevenBits){
 		// If there is an even number of 'BITs' return the complement of BIT
 		if(count(sevenBits) % 2 == 0){
-			return complement();	
+			return COMPLEMENT;	
 		}
 		else{
-			return complement();
-		}
-	}
-	/**
-	 * @return complement of BIT. 
-	 */
-	private String complement(){
-		if(BIT == '0'){
-			return "1";
-		}
-		else{
-			return "0";
+			return BIT;
 		}
 	}
 	/**
@@ -90,8 +79,8 @@ public class KeyGenerator {
 	 */
 	private int count(String sevenBits){
 		int count = 0;
-		for(int i = 0; i < sevenBits.length(); i++){
-			if(sevenBits.charAt(i) == BIT){
+		for(int i = 0; i < sevenBits.length() - 1; i++){
+			if(sevenBits.substring(i, i + 1).equals(BIT)){
 				count++;
 			}
 		}
@@ -107,8 +96,8 @@ public class KeyGenerator {
 		for(int i = 0; i < subkeys.length; i++){
 			c = leftShift(i, leftSide(key));
 			d = leftShift(i, rightSide(key));
-			key = c+d;
-			add(Transposition.permutation(key, PC2));
+			key = c + d;
+			add(Transposition.permute(key, PC2));
 		}
 	}
 	/**
